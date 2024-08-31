19d5d03d4a1f0217af63ae4ad54d91157852f91d
Gabriel <gabrielcarra.p@hotmail.com>
2024-08-31 09:21:13 -0300

    Starter Config commit

---
+++ src/test/java/br/com/demoparkapi/DemoParkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.demoparkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoParkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
